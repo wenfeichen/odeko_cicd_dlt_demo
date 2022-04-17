1f6ba97b49653d3ab34f313b3186ef2aad36557b
Wenfei Chen <wchen@odeko.com>
2022-04-17 08:41:07 -0400

    update project version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
-from odeko_cicd_dlt_demo import __version__
+#from odeko_cicd_dlt_demo import __version__
 
 setup(
     name="odeko_cicd_dlt_demo",
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
-    version=__version__,
+    version=0.0.1,
     description="odeko_cicd_dlt_demo",
     author="wchen@odeko.com",
 )
